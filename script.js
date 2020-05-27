e35b88eb5bb149628e8e61dbb6e60e7ccf0edaa6
Marco Salles <marco.salles@geekie.com.br>
2020-05-27 10:30:33 -0300

    Resolução

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 
 class Usuario {
 	constructor(nome, email, ativo) {
+		this.nome = nome;
+		this.email = email;
+		this.ativo = ativo;
 	}
 }
 
-function criaUsuario() {
-	var usuario;
+var criaUsuario = () => {
+	var inputNome = document.getElementById('nome');
+	var nome = inputNome.value;
+
+	var inputEmail = document.getElementById('email');
+	var email = inputEmail.value;
+
+	var inputAtivo = document.getElementById('ativo');
+	var ativo = 'Não';
+	if (inputAtivo.checked == true) {
+		ativo = 'Sim';
+	}
+
+	var usuario = new Usuario(nome, email, ativo);
 	return usuario;
 }
+
 function cadastrar() {
 	console.log('Cadastro iniciado...');
-	var usuario;
+	var usuario = criaUsuario();
+	console.log(usuario);
+	var corpoDaTabela = document.querySelector('table tbody');
+	var linha = document.createElement('tr');
+	var tdNome = document.createElement('td');
+	tdNome.innerHTML = usuario.nome;
+	var tdEmail = document.createElement('td');
+	tdEmail.innerHTML = usuario.email;
+	var tdAtivo = document.createElement('td');
+	tdAtivo.innerHTML = usuario.ativo;
+
+	linha.append(tdNome);
+	linha.append(tdEmail);
+	linha.append(tdAtivo);
+
+	corpoDaTabela.append(linha);
 }
